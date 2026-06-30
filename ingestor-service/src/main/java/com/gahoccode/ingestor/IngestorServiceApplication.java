fe990bb8599bbc1dc3fcb0eb649652775243ff5c
dominhit215@gmail.com <dominhit215@gmail.com>
2026-07-01 00:48:33 +0700

    chore: initialize ingestor-service module

---
+++ ingestor-service/src/main/java/com/gahoccode/ingestor/IngestorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gahoccode.ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestorServiceApplication.class, args);
+	}
+
+}
